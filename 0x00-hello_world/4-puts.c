c47d815ddd9126756044f8bfd8ba939214833286
priest-tech <shixkamanu@gmail.com>
2022-07-02 23:53:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,6 @@
 #!/bin/bash
 #include<stdio.h>
-
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-
+int main(void) {
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
