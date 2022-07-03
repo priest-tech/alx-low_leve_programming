b079e7b4c3de5309882d00ac303903181c7a4d71
priest-tech <shixkamanu@gmail.com>
2022-07-03 00:32:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #!/bin/bash
 #include<stdio.h>
-int main(void) {
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	puts("with proper grammar, but the outcome is a piece of art,");
 	return(0);
 }
