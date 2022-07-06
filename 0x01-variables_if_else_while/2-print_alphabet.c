d7fd1428948a692dd67a158a2e77bbb66ba2a0c8
priest-tech <shixkamanu@gmail.com>
2022-07-06 02:35:46 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 00 to 99.
+ * main - Prints the alphabet in lowercase.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i, e;
+	char letter;
 
-	i = 48;
-	e = 48;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-	while (e < 58)
-	{
-		i = 48;
-		while (i < 58)
-		{
-			putchar(e);
-			putchar(i);
-			if (i == 57 && e == 57)
-			{
-				break;
-			}
-			putchar(',');
-			putchar(' ');
-			i++;
-		}
-		e++;
-	}
 	putchar('\n');
+
 	return (0);
 }
